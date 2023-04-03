be8a291532fb6da41e733b0d133d7811a9b41173
Jeramaya Gramby <jeramayagramby@gmail.com>
2023-04-03 02:21:06 -0400

    Add DAG dependencies

---
+++ airflow_dags/facebook_dag.py
@@ -0,0 +1,7 @@
+from datetime import timedelta
+from airflow import DAG 
+from airflow.operators.python import PythonOperator
+from airflow.utils.dates import days_ago
+from datetime import datetime
+import sys
+import logging as log
